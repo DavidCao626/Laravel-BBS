f124d525a75f924b7654f219d625804f963096c1
David Cao <DavidCao626@foxmail.com>
2017-11-15 02:41:49 +0800

    Create categories

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,5 +1,4 @@
-<nav class="navbar navbar-default navbar-static-top">
-    <div class="container">
+<nav class="navbar navbar-default navbar-static-top"> <div class="container">
         <div class="navbar-header">
 
             <!-- Collapsed Hamburger -->
@@ -12,7 +11,7 @@
 
             <!-- Branding Image -->
             <a class="navbar-brand" href="{{ url('/') }}">
-                LaraBBS
+                编程达人
             </a>
         </div>
 
@@ -32,7 +31,7 @@
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                             <span class="user-avatar pull-left" style="margin-right:8px; margin-top:-5px;">
-                                <img src="{{ Auth::user()->avatar }}" class="img-responsive img-circle" width="30px" height="30px">
+                                <img src="{{ Auth::user()->avatar }}" class="img-responsive img-circle" width="30px" style="height:35px">
                             </span>
                             {{ Auth::user()->name }} <span class="caret"></span>
                         </a>
