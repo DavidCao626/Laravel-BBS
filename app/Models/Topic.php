98648793eebcd7d690ff473cf6093a98dbc9b7d9
DavidCao <davidcao@DavidCaodeiMac.lan>
2017-11-15 14:59:29 +0800

    List user replies

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -4,9 +4,12 @@ namespace App\Models;
 
 class Topic extends Model
 {
-    protected $fillable = [
-        'title', 'body', 'category_id', 'excerpt', 'slug'
-    ];
+    protected $fillable = ['title', 'body', 'category_id', 'excerpt', 'slug'];
+
+      public function replies()
+      {
+          return $this->hasMany(Reply::class);
+      }
 
     public function category()
     {
@@ -52,4 +55,6 @@ class Topic extends Model
     {
         return route('topics.show', array_merge([$this->id, $this->slug], $params));
     }
+
+
 }
